c7c9cfc9433b6cc08c82d09eb60f90910507c0a3
mzachner <mzachner@users.noreply.github.com>
2015-03-13 15:05:03 +0100

    neue dependency für die neue jsf 2.2 Funktionalität Fileupload.

--- Zachners/src/main/java/at/zachner/pages/HelloBean.java
+++ Zachners/src/main/java/at/zachner/pages/HelloBean.java
@@ -2,8 +2,11 @@ package at.zachner.pages;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.servlet.http.Part;
 
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.Scanner;
 
 @ManagedBean
 @SessionScoped
@@ -20,5 +23,29 @@ public class HelloBean implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	
+	
+	
+	
+	private Part file;
+	private String fileContent;
+
+	public void upload() {
+	  try {
+	    fileContent = new Scanner(file.getInputStream()).useDelimiter("A").next();
+	  } catch (IOException e) {
+	  	// Error handling
+	  }
+	}
+
+	public Part getFile() {
+	  return file;
+	}
+
+	public void setFile(Part file) {
+	  this.file = file;
+	}
+	
 
 }
