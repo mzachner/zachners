b7e7abcabc8d331e4051c41c93b5cfae55c9f0a3
mzachner <mzachner@users.noreply.github.com>
2015-03-17 07:12:11 +0100

    Lebenslauf Übung

--- Zachners/src/main/java/at/zachner/pages/HelloBean.java
+++ Zachners/src/main/java/at/zachner/pages/HelloBean.java
@@ -1,12 +1,9 @@
 package at.zachner.pages;
 
+import java.io.Serializable;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.servlet.http.Part;
-
-import java.io.IOException;
-import java.io.Serializable;
-import java.util.Scanner;
 
 @ManagedBean
 @SessionScoped
@@ -23,29 +20,5 @@ public class HelloBean implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	
-	
-	
-	
-	private Part file;
-	private String fileContent;
-
-	public void upload() {
-	  try {
-	    fileContent = new Scanner(file.getInputStream()).useDelimiter("A").next();
-	  } catch (IOException e) {
-	  	// Error handling
-	  }
-	}
-
-	public Part getFile() {
-	  return file;
-	}
-
-	public void setFile(Part file) {
-	  this.file = file;
-	}
-	
 
 }
