0a309672f4d5da0720f41224555d4a4d641ea74e
mzachner <mzachner@users.noreply.github.com>
2015-05-06 20:17:42 +0200

    Filter

---
+++ Zachners/src/main/java/at/zachner/filter/AnyPageFilter.java
@@ -0,0 +1,45 @@
+package at.zachner.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+public class AnyPageFilter implements Filter {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+
+		HttpServletRequest req = (HttpServletRequest) request;
+		
+		String requestedSessionId = req.getRequestedSessionId();
+		
+		boolean valid = req.isRequestedSessionIdValid();
+		
+		HttpSession session = req.getSession();
+		
+		String sessionId = session.getId();
+		chain.doFilter(request, response);
+		
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
